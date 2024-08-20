9d88b4cff2467c832b738e9dbfcb6168974dc05a
RuiZhao <Rui.Zhao@metropolia.fi>
2024-08-20 09:17:20 +0300

    Merge remote-tracking branch 'origin/main'

--- Exercise 2/Exercise_2_4.py
+++ Exercise 2/Exercise_2_4.py
@@ -1,14 +1,15 @@
 print("Please enter three integers: ")
-num1_input = input()
-num2_input = input()
-num3_input = input()
-
+num1_input = input('Enter the first number:\n')
 num1 = int(num1_input)
+num2_input = input('Enter the second number:\n')
 num2 = int(num2_input)
+num3_input = input('Enter the third number:\n')
 num3 = int(num3_input)
 
 numbers_sum = num1 + num2 + num3
+numbers_product = num1 * num2 * num3
 numbers_average = numbers_sum / 3
 
 print("The sum of the three integers is: " + str(numbers_sum))
+print("The product of the three integers is: " + str(numbers_product))
 print("The average of the three integers is: " + str(numbers_average))
